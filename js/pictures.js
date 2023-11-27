8f0b49f7f5bb42dbc3d69241f59f1a4a68b89613
generalgrishus <131965815+generalgrishus@users.noreply.github.com>
2023-11-28 00:49:10 +0500

    8.14

--- js/pictures.js
+++ js/pictures.js
@@ -1,21 +1,27 @@
-const pictures = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+const pictureTemplate = document.querySelector('#picture').content.querySelector('a');
 
-const renderPhoto = (photo) => {
+const renderPhoto = (picture) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('img').alt = photo.description;
-  pictureElement.querySelector('img').src = photo.url;
-  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
-  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  return pictureElement;
+
+  pictureElement.querySelector('img').src = picture.url;
+  pictureElement.querySelector('img').alt = picture.description;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  pictureElement.dataset.id = picture.id;
+
+  fragment.append(pictureElement);
 };
 
-const renderPhotos = (photos) => {
-  const fragment = document.createDocumentFragment();
-  photos.forEach((photo) => {
-    fragment.appendChild(renderPhoto(photo));
+const renderPhotos = (pictures) => {
+
+  const pictureContainer = document.querySelector('.pictures');
+  pictures.forEach((picture) => {
+    renderPhoto(picture);
   });
-  pictures.appendChild(fragment);
+
+  pictureContainer.append(fragment);
 };
 
 export {renderPhotos};
