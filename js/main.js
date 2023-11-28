2d53af07249aa9033b9709c7b0516b598d1676e3
generalgrishus <131965815+generalgrishus@users.noreply.github.com>
2023-11-28 13:14:19 +0500

    8.15

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { photos } from './data.js';
 import { renderPhotos } from './pictures.js';
-import { initPictures } from './big-picture.js';
+import { initializePictures } from './big-picture.js';
 
 renderPhotos(photos);
 
-initPictures(photos);
+initializePictures(photos);
