7d9197d555c93577a51e7323fcafae69fb8b1561
Keks <keks@htmlacademy.ru>
2023-12-19 11:24:25 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,7 @@
+import { photos } from './data.js';
+import { createPictures } from './pictures.js';
+import './big-picture.js';
+import './form.js';
+import './validate-form.js';
+
+createPictures(photos);
