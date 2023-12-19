72c207bd0a027cfe096b5e41fdbc337d2b7fff7a
generalgrishus <131965815+generalgrishus@users.noreply.github.com>
2023-12-19 17:56:32 +0500

    9.15

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { photos } from './data.js';
 import { createPictures } from './pictures.js';
 import './big-picture.js';
-import './form.js';
+import { initForm } from './form.js';
 import './validate-form.js';
+import './effects.js';
 
 createPictures(photos);
+initForm();
