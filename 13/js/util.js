18610aeb69a0d168ec53453479c07a2df2d3a1b9
Keks <keks@htmlacademy.ru>
2023-12-20 20:22:05 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/util.js
@@ -0,0 +1,21 @@
+const getRandomInt = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscKey = (evt) => evt.key === 'Escape';
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+
+export {getRandomInt, isEscKey, debounce, shuffle};
